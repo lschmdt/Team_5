f5660c3d1115fa27c4a561f103851d4f23b465ea
lhodac <lea.hodac@epfl.ch>
2017-11-14 12:58:54 +0100

    Add gtest file

---
+++ build/CMakeFiles/Generations.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/Lea/Desktop/Team_5/src/Allele.cpp" "/Users/Lea/Desktop/Team_5/build/CMakeFiles/Generations.dir/src/Allele.cpp.o"
+  "/Users/Lea/Desktop/Team_5/src/Generation.cpp" "/Users/Lea/Desktop/Team_5/build/CMakeFiles/Generations.dir/src/Generation.cpp.o"
+  "/Users/Lea/Desktop/Team_5/src/Simulation.cpp" "/Users/Lea/Desktop/Team_5/build/CMakeFiles/Generations.dir/src/Simulation.cpp.o"
+  "/Users/Lea/Desktop/Team_5/src/main.cpp" "/Users/Lea/Desktop/Team_5/build/CMakeFiles/Generations.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../gtest/include"
+  "../gtest"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/Generations.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Generations.dir/src/Allele.cpp.o"
+  "CMakeFiles/Generations.dir/src/Generation.cpp.o"
+  "CMakeFiles/Generations.dir/src/Simulation.cpp.o"
+  "CMakeFiles/Generations.dir/src/main.cpp.o"
+  "Generations.pdb"
+  "Generations"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Generations.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
