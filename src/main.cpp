1ceb369d8d5e5238d4aa1c0999fd7609568aed06
lululafripouille <32455845+lululafripouille@users.noreply.github.com>
2017-11-15 09:40:32 +0100

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,11 @@
 #include "Simulation.hpp"
 #include "Generation.hpp"
 
-int main(int argc, char**argv)
-{
-	
+int main(int argc, char**argv) {
+	Simulation S ({new Generation({"a","b","a","a","b","a","c","b"})});
+	S.printTerminal();
+	std::cout << "tout va mal" << std::endl;
+	S.createNewGeneration();
+	S.printTerminal();
 	return 0;
 }
