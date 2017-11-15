4d95fa766200f418d28819e8d0bef63c0869253f
cykunz <32450513+cykunz@users.noreply.github.com>
2017-11-15 17:47:56 +0100

    Update of Comments

--- src/Allele.hpp
+++ src/Allele.hpp
@@ -7,8 +7,8 @@
 
 /*! \class Allele
  * 
- * this class gathers the characteristics of each allele 
- * retrieved from the data base
+ * This class gathers the characteristics of each allele 
+ * retrieved from the data base. They each have a specific sequence as well as a frequency. 
  * 
  */
  
@@ -17,32 +17,33 @@ class Allele
 {
 	private: 
 	
-		std::string sequence_;
-		double frequency_; 
+		std::string sequence_; 		/**<	Sequence of allele.				*/
+		double frequency_; 		/**<	Frequency of allele within a generation.	*/
 				
-		/** retrieves the DNA sequence of the alleles contained 
-		 * in the genotype-database and initialises the attribut sequences_
-		 *   
-		 * */ 
-		void readFromFile(std::ostream& Data);
 	
 	public: 
 	
-		/** \fn allele constructor 
+		/** \fn Allele constructor, initialized an Allele with a given sequence and frequency.
 		 *
 		 * */ 
 		Allele(std::string sequence, double frequency); 
 		
-		/** allele destructor 
+		/** Allele destructor 
 		 *  
 		 * 
 		 * */  
 		 ~Allele(); 
 		 
+		/**	Getter of the allele's sequence.
+		*	@return string: Allele sequence.	*/
 		 std::string getSequence();
-
+		
+		/**	Getter of the allele's frequency.
+		*	@return double: Allele frequency.	*/
 		 double getFrequency();
 		 
+		/**	Setter of the allele's frequency.
+		*	@param freq: New frequency of the given allele.	*/
 		 void setFrequency(double freq);
 
 };
