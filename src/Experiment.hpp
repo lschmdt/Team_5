1a91ad74615003df0e0f5bd304eb57e4270e24f1
cykunz <32450513+cykunz@users.noreply.github.com>
2017-11-16 18:43:28 +0100

    New experiment class

---
+++ src/Experiment.hpp
@@ -0,0 +1,53 @@
+#ifndef EXPERIMENT_H
+#define EXPERIMENT_H
+#include "Simulation.hpp"
+#include <vector>
+#include <fstream>
+#include <memory>
+
+using namespace std;
+
+/*! \class Experiment
+ * 	Represents an overall number of simulations that will run simultaneously.
+ * 
+ */
+ 
+class Experiment 
+{	
+	private:
+	
+	vector<Simulation*> simulations_;	/**<	All simulations contained in the experiment.	*/
+	
+	ofstream output_file_;				/**<	External file where frequencies and genotypes will be written.	*/
+	
+	
+	
+	public:
+	/**	Constructor of experiment, initialized with a vector of Simulations.	*/
+	Experiment(vector<Simulation*> sim);
+	
+	/**	Destructor of Experiment.	*/
+	~Experiment();
+	
+	/**	Public function updating all simulations contained in the overall experiment
+	 * 	simultaneously for a given number of time steps.
+	 * 	@param time: Time during which the simulation will run. 	*/
+	void runall(size_t time);
+	
+	/**	A public function writing allele frequencies in an external file.
+	* @param ofstream: External file.
+	* @param Simulation*: Simulation wanting to be written in an external file.	*/
+	void writeFrequencies(std::ofstream& Output, Simulation* sim);
+	
+	/**	A public function writing allele genotypes in a file.
+	* 	@param ofstream: External file.	*/
+	void writeGenotypes(std::ofstream& Output);
+	
+	
+	
+};
+
+
+
+
+#endif
