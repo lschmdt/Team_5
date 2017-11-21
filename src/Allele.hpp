b97ebc8c459f9dcba93e305e1cf08f9ae0988f14
Cléa Kunz <clea.kunz@epfl.ch>
2017-11-21 17:27:04 +0100

    Update of Allele with fitness coefficient

--- src/Allele.hpp
+++ src/Allele.hpp
@@ -3,7 +3,7 @@
 
 #include <vector>
 #include <fstream> 
-
+#include <random>
 
 /*! \class Allele
  * 
@@ -16,17 +16,19 @@
 class Allele
 {
 	private: 
-	
+
 		std::string sequence_; 		/**<	Sequence of allele.				*/
 		double frequency_; 		/**<	Frequency of allele within a generation.	*/
-				
-	
+		double fitness_;		/**<	Fitness coefficient of allele.	*/
+		std::mt19937 generator;
+		std::uniform_real_distribution<double> distribution;
+		
 	public: 
 	
 		/** \fn Allele constructor, initialized an Allele with a given sequence and frequency.
 		 *
 		 * */ 
-		Allele(std::string sequence, double frequency); 
+		Allele(std::string sequence, double frequency, double fit=0.0); 
 		
 		/** Allele destructor 
 		 *  
@@ -34,18 +36,24 @@ class Allele
 		 * */  
 		 ~Allele(); 
 		 
+		 /**	Getter of allele's fitness coefficient.
+		  * 	@return double: Fitness coefficient.	*/
+		double getFitness() const;
 		/**	Getter of the allele's sequence.
 		*	@return string: Allele sequence.	*/
 		 std::string getSequence() const;
-		
+		 
 		/**	Getter of the allele's frequency.
 		*	@return double: Allele frequency.	*/
 		 double getFrequency() const;
 		 
+		 /** Setter of allele's fitness coefficient.
+		 *  @param fit: New fitness coefficient.	*/
+		 void setFitness(double const& fit);
+		 
 		/**	Setter of the allele's frequency.
 		*	@param freq: New frequency of the given allele.	*/
 		 void setFrequency(double const& freq);
-
 };
 
 #endif
