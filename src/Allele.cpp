cd0344bf36555c1385e865868bfa46a568593616
Cléa Kunz <clea.kunz@epfl.ch>
2017-11-21 17:27:33 +0100

    Update of Allele

--- src/Allele.cpp
+++ src/Allele.cpp
@@ -2,11 +2,22 @@
 
 using namespace std;
 
-Allele::Allele(string sequence, double frequency)
-: sequence_(sequence), frequency_(frequency) {}
+Allele::Allele(string sequence, double frequency, double fit)
+: sequence_(sequence), frequency_(frequency), fitness_(fit) {
+	if(fit==0.0)
+	{	random_device rd;
+		generator=mt19937(rd());
+		distribution=uniform_real_distribution<double>(-1.0,1.0);
+		setFitness(distribution(generator));
+		
+	}
+}
 	 
 Allele::~Allele(){}
     
+double Allele::getFitness() const{
+	return fitness_;
+}
 
 std::string Allele::getSequence() const{
 	return sequence_;
@@ -15,6 +26,10 @@ std::string Allele::getSequence() const{
 double Allele::getFrequency() const{
 	return frequency_;
 } 
+
+void Allele::setFitness(double const& fit){
+	fitness_=fit;
+}
 
 void Allele::setFrequency(double const& freq) {
 	frequency_ = freq;
