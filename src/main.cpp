d68e5dbccce0b0944e3b265bdbd3dd9b719ce940
lhodac <32448517+lhodac@users.noreply.github.com>
2017-11-28 14:19:57 +0100

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,15 @@
 using namespace TCLAP;
 
 
+
 int main( int argc, char **argv ) {
 	
 	
 	std::vector<int> markers;
 	std::vector<double> freq;
+	bool selection;
+	bool sizeModification;
+	bool mutate;
 	
 	try{
 		CmdLine cmd( "Ask the marker site to the user" );
@@ -24,10 +28,20 @@ int main( int argc, char **argv ) {
 	
 		MultiArg<double> frequencies("f", "frequencies", "Give me some frequencies", false, "double");
 		cmd.add(frequencies);
+		
+		SwitchArg selectionSwitch("selec", "selection", "Active the selection switch", cmd, false);
+		SwitchArg sizeModifSwitch("size", "population size modification", "Active the population size modification switch", cmd, false);
+		SwitchArg mutationSwitch("mutate","mutation", "Active the mutation switch", cmd, false);
+
+		
 		cmd.parse(argc, argv);
 	
 		for (auto f : frequencies.getValue()) {freq.push_back(f);}
 		for (auto n : markersPositions.getValue()) {markers.push_back(n);}
+		selection=(selectionSwitch.getValue());
+		sizeModification=(sizeModifSwitch.getValue());
+		mutate=(mutationSwitch.getValue());
+		
 		//assert((int)freq.size() == nb_alleles.getValue()); 
 		
 	} catch(TCLAP::ArgException &e) {std::cerr << "ERROR: " << e.error() << " for arg " << e.argId() << std::endl;}
@@ -38,7 +52,7 @@ int main( int argc, char **argv ) {
 	
 		if (markers.empty())
 		{
-			Simulation S1(freq); 
+			Simulation S1(freq, selection, sizeModification, mutate); 
 			
 			S1.printTerminal();	
 			for (int i(0); i < 6 ; ++i) {
@@ -46,12 +60,12 @@ int main( int argc, char **argv ) {
 			}
 			S1.printTerminal();
 			
-			std::vector<Simulation*> sim1 = std::vector<Simulation*> {new Simulation(freq), new Simulation(freq), new Simulation(freq)};
+			std::vector<Simulation*> sim1 = std::vector<Simulation*> {new Simulation(freq, selection, sizeModification, mutate), new Simulation(freq, selection, sizeModification, mutate), new Simulation(freq, selection, sizeModification, mutate)};
 			
 			Experiment exp(sim1);
 			exp.runall(300);
 		} else {
-			Simulation S2(markers,false, true);
+			Simulation S2(markers, selection, sizeModification, mutate);
 			
 			S2.printTerminal();	
 			for (int i(0); i < 6 ; ++i) {
@@ -59,7 +73,7 @@ int main( int argc, char **argv ) {
 			}
 			S2.printTerminal();
 			
-			std::vector<Simulation*> sim2 = std::vector<Simulation*>{new Simulation(markers), new Simulation(markers), new Simulation(markers)};	
+			std::vector<Simulation*> sim2 = std::vector<Simulation*>{new Simulation(markers, selection, sizeModification, mutate), new Simulation(markers, selection, sizeModification, mutate), new Simulation(markers, selection, sizeModification, mutate)};	
 		
 			Experiment exp(sim2);
 			exp.runall(300);
@@ -73,6 +87,18 @@ int main( int argc, char **argv ) {
 		std::cerr << e << std::endl;
 		return 1;
 		}
+	
+	/*
+	Simulation sim(new Generation({"ACC","GCC","CCC","GCC","GCC","GCC","CCC","CCC","GCC","GCC"}, {0.5, 0.5, 0.5}), false,false, true);
+	std::cout << "Fin construction simulation" << std::endl;
+
+	for (int i(0); i < 50 ; ++i) {
+		sim.createNewGeneration();
+	}
+	std::cout << "Fin create newGeneration" << std::endl;
+	sim.printTerminal();*/
+	
+	
 	
 	return 0;
 }
